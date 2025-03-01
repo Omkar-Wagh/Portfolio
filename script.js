83f2ffaef0265c636b5dd6d860e8f990c5fa3518
Omkar Wagh <167704070+Omkar-Wagh@users.noreply.github.com>
2025-03-01 14:54:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -54,3 +54,17 @@ document.getElementById("contact-form").addEventListener("submit", async functio
         alert("❌ Error sending message. Please try again.");
     }
 });
+
+const block2 = document.getElementById('block2');
+
+window.addEventListener('scroll', () => {
+    const scrollPercentage = (window.scrollY / (document.body.offsetHeight - window.innerHeight)) * 100;
+    
+    if (scrollPercentage >= 90) {
+        block2.style.overflowX = 'auto';
+        block2.style.overflowY = 'hidden';
+    } else {
+        block2.style.overflowX = 'hidden';
+        block2.style.overflowY = 'auto';
+    }
+});
